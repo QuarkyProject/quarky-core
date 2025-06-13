6ed7ebb3c42f4a0ec359ec94518cb635b6d6edcf
Serhat Başesgioğlu <55340640+SerhatBasesgioglu@users.noreply.github.com>
2025-06-13 23:26:35 +0300

    feat(notorious): add dummy note controller

---
+++ src/core/Controllers/NotesController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Quarky.Core.Entities;
+
+namespace Quarky.Core.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class NotesController : ControllerBase
+{
+    private static List<Note> _noteList = new();
+    private Note note = new Note { Content = "yo", Title="Nice", Id="1", CreatedAt=DateTime.Now };
+
+
+    [HttpGet]
+    public List<Note> Get()
+    {
+        _noteList.Add(note);
+        return _noteList;
+    }
+
+    [HttpDelete]
+    public void DeleteAll()
+    {
+        _noteList.Clear();
+    }
+}
+
---
+++ src/core/Entities/Note.cs
@@ -0,0 +1,10 @@
+﻿namespace Quarky.Core.Entities;
+
+public class Note
+{
+    public string Id { get; set; }
+    public string Title { get; set; }
+    public string Content {  get; set; }
+    public DateTime CreatedAt { get; set; }
+    public DateTime? UpdatedAt { get; set; }
+}
